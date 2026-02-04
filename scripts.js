501e424d332687bf7fa7a3a97601f4f8aa57bb7b
Carlos Eduardo Araujo <carlosaraujo.dev@gmail.com>
2026-02-04 18:58:04 -0300

    add input event listener to amount field for number input

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const amount = document.getElementById("amount")
+
+// Manipulando o input amount pare receber somente números
+amount.addEventListener("input", ()=>{
+    console.log(amount.value)
+})
